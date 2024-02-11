ff0408395d40302c9ad4634fa4d200d57710d444
toky666 <99110959+toky666@users.noreply.github.com>
2024-02-11 01:39:14 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+    /*----------------------------
+    		Video popup active
+    	------------------------------*/
+
+    $(".popup-video").magnificPopup({
+        type: "iframe"
+    });
+    new WOW().init();
+
+})(jQuery);
